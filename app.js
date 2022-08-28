853e6d89cbb6dcd27a9d67a5a63c0bcd5c4d10cb
Saatvik1412 <108618321+Saatvik1412@users.noreply.github.com>
2022-08-28 23:14:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const JWT_SECRET =
   const mongoUrl="mongodb+srv://starlord:linkinpa@cluster0.sk1bjdg.mongodb.net/?retryWrites=true&w=majority"
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Origin", "https://mybooklist-web.netlify.app/")
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested, Content-Type, Accept Authorization"
